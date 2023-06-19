71cb0154f2ebf0de5ffe00ae218c0d830d124e52
paleto30 <paleto0817@gmail.com>
2023-06-19 18:10:24 -0500

    feat (full functions): :rocket: endpoint listo para registrar facturas
    se implementaron funciones de falidacion en el modelo ,
    estas funciones me permiten ahcer todas las validaciones necesarias para que no falle un registro
    
    tembien en la funcion "crearFacturacionDb()" se hicieron todas las validaciones petinentes para que antes de enviar un registro a la base de datos primero se hayan validado todos los posibles errores,
    
    una vez comple con todas las validaciones sera posible
    crear una factura, un usuario o agregarle una nueva factura a un usuario existente y unos detalles de factura con los respectivos productos de la factura ,
    adicionalmente se esta haciendo un descuento del stock en la tabla de productos, es decir si se compra un producto este descuenta la cantidad que  se compro  y para fucturas compras si el producto no cuenta con un stock suficiente para las unidades solicitaddas enviara un mensaje en el que especifica las unidades dispobles del producto.
    
    a mi parecer estan listas todas las posibles validaciones ✨.

--- app/Models/ModeloCrearFacturacion.php
+++ app/Models/ModeloCrearFacturacion.php
@@ -32,19 +32,112 @@ class ModeloCrearFacturacion{
         ];
     }
 
+
+    /* 
+            funcion me permite validar por la cedula
+            que el cliente no este registrado  
+    */ 
+    private function validarCliente($cedula){
+        try {
+
+            $db = new Conexion;
+            $query = "select * from clientes where cedula = ?";
+            $stament = $db->connect()->prepare($query);    
+            $stament->execute([$cedula]);
+            $clienteExiste = $stament->fetch(PDO::FETCH_ASSOC);
+            $db->closedCon();
+            if (empty($clienteExiste))  return false; // si el cliente no existe retorna falso, (osea que no existe)
+
+            return true;// si el cliente existe entonces returna true (osea que existe)
+        } catch (\Exception $e) {
+            return  $e->getMessage();
+        }
+    }
     
 
+    /* 
+            funcion para obtener los datos del cliente existente 
+    */
+    private function obtenerCliente($cedula){
+        try {
+            $db = new Conexion;
+            $query = "select * from clientes where cedula = ?";
+            $sta = $db->connect()->prepare($query);
+            $sta->execute([$cedula]);
+            $cliente = $sta->fetch(PDO::FETCH_ASSOC);
+            $db->closedCon();
+            return $cliente;
+        } catch (\Exception $e) {
+            return  $e->getMessage();
+        }
+    }
+
+
+    /*  
+            funcion para validar la que la factura no haya sido 
+            creada antes, si esta ya fue creada antes entonces
+            no se podra registrar
+    */
+    public function validarFactura($codigoFact){
+        try {
+
+            $db = new Conexion;
+            $query = "select * from facturas where codigo_factura = ?";
+            $stament = $db->connect()->prepare($query);    
+            $stament->execute([$codigoFact]);
+            $facturaExiste = $stament->fetch(PDO::FETCH_ASSOC);
+            $db->closedCon();
+            if (empty($facturaExiste))  return false;  // si el cliente no existe retorna falso, (osea que no existe)
+               
+            return true;  // si el cliente existe entonces returna true (osea que existe)
+        } catch (\Exception $e) {
+            return  $e->getMessage();
+        }
+    }
+
+
     /**  
-     *       funcion que me permite realizar una facturacion 
-     *      esta funcion recibe todos los datos , y utiliza las diferentes funciones de los modelos
-     *       para realizar los guardados de los datos en la base de datos
-     */    
+     *       funcion encargada de usar todas las funciones de validacion anteriores 
+     *       una vez se hacen todass las validaciones esta es la funcion
+     *       que me va a permitir crear un registro de una facturacion,
+     *       y va a registrar los datos en las respectivas tablas que esten 
+     *       implicadas en la accion de una facturacion 
+     */
     public function crearFacturacionDb()
     {
         
         try {
 
             $db = new Conexion;
+
+
+            // for para validar que los productos tengas todas sus llaves y valores con datos
+            foreach ($this->productos as $key) {
+                foreach ($key as $k => $v) {
+                    if (empty($k) || empty($v)) {
+                        return [
+                            'ERROR' => "NI LA LLAVE NI EL VALOR DEL 'PRODUCTO' PUEDEN SER VACIOS!"
+                        ];
+                    }
+                }
+            }
+
+            // for para validar que las llaves del cod_producto no tengan un valor
+            // repetido 
+            $valoresCod_producto = [];
+            foreach ($this->productos as $key) {
+                foreach ($key as $k => $v) {
+                    if($k === 'cod_producto'){
+                        if (in_array($v,$valoresCod_producto)) {
+                            return [
+                                'error' => "los valores de cod_producto  no deben repetirse, Puedes aumentar la cantidad sin necesidad de registrarlo dos veces",
+                                'message' => "campo duplicado en cod_producto: $v"
+                            ];
+                        }
+                        $valoresCod_producto[] = $v;
+                    }
+                }
+            }
 
 
             // for  para la validacion de la existencia y stock de los productos 
@@ -66,42 +159,66 @@ class ModeloCrearFacturacion{
                 }
             }
             
-
-            $cliente = new ClienteModel($this->dataCliente['cedula'],$this->dataCliente['nombre'],$this->dataCliente['correo'],$this->dataCliente['direccion'],$this->dataCliente['telefono']);
-            $clienteCreado = $cliente->addCliente();
-
-            if (empty($clienteCreado)) {
-                return ['message' => 'Algo ha salido Mal, revisa  la informacion que intentas registrar'];
+            // for para validar que los campos de la factura no vengan vacios
+            foreach ($this->dataFactura as $key => $value) {
+                if (empty($value)) {
+                    return ['message' => 'Campos de la factura vacios'];
+                }
+            }
+            
+            // for para validad que los campos del cliente no vengan vacios
+            foreach ($this->dataCliente as $key => $value) {
+                if (empty($value)) {
+                    return ['message' => 'Campos del cliente vacios'];
+                }
             }
 
-            $factura = new FacturaModel($this->dataFactura['codigo_factura'],$this->dataFactura['nombre_vendedor'],$this->dataFactura['fecha'], $clienteCreado[0]['id']);
-            $facturaCreada = $factura->insertAbill();
-
-            if (empty($facturaCreada)) {
-                return ['message' => 'Error en la creacion de la factura'];
+            // rompemos si la facttura ya existe , debido a que  no pueden haber dos facturas con el mismo codigo 
+            if ($this->validarFactura($this->dataFactura['codigo_factura'])) {
+                return ['error' => 'El codigo de esta factura ya existe, no pueden existir dos facturas con el mismo codigo'];
             }
+            
+            // si el cliente ya existe entonces , no se crea un nuevo cliente
+            // se hace la factura con los datos del cliente existente
+            if ($this->validarCliente($this->dataCliente['cedula'])) {
+                $clienteCreado = $this->obtenerCliente($this->dataCliente['cedula']);
+                // creamos la factura
+                $factura = new FacturaModel($this->dataFactura['codigo_factura'],$this->dataFactura['nombre_vendedor'],$this->dataFactura['fecha'], $clienteCreado['id']);
+                $newFactura = $factura->insertAbill();
 
+            }else{
+
+                // creamos el cliente y pasamos los campos correspondientes
+                $cliente = new ClienteModel($this->dataCliente['cedula'],$this->dataCliente['nombre'],$this->dataCliente['correo'],$this->dataCliente['direccion'],$this->dataCliente['telefono']);
+                $clienteCreado = $cliente->addCliente();
+                
+                // creamos la factura
+                $factura = new FacturaModel($this->dataFactura['codigo_factura'],$this->dataFactura['nombre_vendedor'],$this->dataFactura['fecha'], $clienteCreado['id']);
+                $newFactura = $factura->insertAbill();
+   
+            }
+            
+            
             foreach ($this->productos as $key) {
                 
                 $query = "select id, stock from productos where cod_producto = ?";
                 $stament = $db->connect()->prepare($query);
                 $stament->execute([$key['cod_producto']]);
-                $product = $stament->fetchAll(PDO::FETCH_ASSOC);
-                $detalle = new DetalleFacturaModel($facturaCreada[0]['id'],$product[0]['id'],$key['cantidad']);
+                $product = $stament->fetch(PDO::FETCH_ASSOC);
+                $detalle = new DetalleFacturaModel($newFactura['id'],$product['id'],$key['cantidad']);
 
-                $product[0]['stock'] -= $key['cantidad'];
+                $product['stock'] -= $key['cantidad'];
                 $queryUpdate = "UPDATE productos SET stock = ? WHERE id = ?";
                 $staments = $db->connect()->prepare($queryUpdate); 
-                $staments->execute([$product[0]['stock'], $product[0]['id']]);
+                $staments->execute([$product['stock'], $product['id']]);
                 $detalle->save();
-            
             }
 
             $db->closedCon();
             return [
-                $clienteCreado[0],
-                $facturaCreada[0],
-                $this->productos
+                'datos Reegistrados' => [
+                    $newFactura,$clienteCreado,$this->productos
+                ]
             ];
 
 
