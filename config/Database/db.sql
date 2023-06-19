26e215bc2f570e2752d1c68319962b17c45cc60f
paleto30 <paleto0817@gmail.com>
2023-06-18 22:10:36 -0500

    feat (consultas):  :zap: consultas utiles
    se modificaron 2  reglones del script , que habia que eliminar ,
    
    se dejo en un comentario 2 consultas que seran utiles  para sacar total de facturas y productos correspondientes a la factura

--- config/Database/db.sql
+++ config/Database/db.sql
@@ -25,7 +25,6 @@ CREATE TABLE facturas(
     nombre_vendedor VARCHAR(255) NOT NULL COMMENT "nombre vendedor",
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT "fecha creacion",
     id_cliente int NOT NULL COMMENT "fk del tabla cliente",
-    total FLOAT NOT NULL COMMENT "precio total de la factura",
     CONSTRAINT fk1_facturas FOREIGN KEY (id_cliente) REFERENCES clientes(id)
 );
 
@@ -33,6 +32,30 @@ CREATE TABLE detalle_factura(
     id int PRIMARY KEY AUTO_INCREMENT COMMENT "llave primaria de factura",
 	id_factura INT NOT NULL COMMENT "id de factura",   
     id_producto INT NOT NULL COMMENT "id de producto",
+    cantidad INT NOT NULL COMMENT "cantidad de productos",
     CONSTRAINT fk1_detalles_factura FOREIGN KEY (id_factura) REFERENCES facturas(id),
     CONSTRAINT fk2_detalles_factura FOREIGN KEY (id_producto) REFERENCES productos(id)
 );
+
+
+
+
+/*     -- consulta para obtener el id y nombre del cliente que tiene una determinada factura
+
+        SELECT clientes.id, clientes.nombre,facturas.codigo_factura, facturas.fecha, SUM(detalle_factura.cantidad) as 'cantidad productos', 
+        sum(productos.valor_unidad * detalle_factura.cantidad) as 'precio total' 
+        FROM clientes, facturas,detalle_factura, productos 
+        WHERE clientes.id = facturas.id_cliente 
+        AND facturas.id = detalle_factura.id_factura 
+        AND detalle_factura.id_producto = productos.id 
+        AND facturas.codigo_factura = 'F0001';
+
+
+        -- consulta para obtener los productos relacionados a una factura en concreto
+
+        SELECT productos.cod_producto , productos.nombre_producto, detalle_factura.cantidad , productos.valor_unidad 
+        from facturas, detalle_factura, productos 
+        WHERE facturas.id = detalle_factura.id_factura 
+        AND detalle_factura.id_producto = productos.id 
+        AND facturas.codigo_factura = 'F0001';
+*/
