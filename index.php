06dd9f8300b9a0957322dfe782dbc5d68586fe6f
paleto30 <paleto0817@gmail.com>
2023-06-20 09:16:33 -0500

    feat (cors): :rocket: header de cors
    se agrego el header de cors para que puedan consultar los datos desde javascript

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
 header("Content-Type:  application/json");
 require_once './vendor/autoload.php';
 require_once './routes/routes.php';
